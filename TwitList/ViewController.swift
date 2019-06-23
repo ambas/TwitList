5a9113c6a58d1b568f8f6203587c4e9decc4494e
Ambas <ambaschobsanti@gmail.com>
2019-06-23 13:40:09 +0700

    Initial Commit

---
+++ TwitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitList
+//
+//  Created by Ambas Chobsanti on 23/6/19.
+//  Copyright © 2019 Ambas Chobsanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
